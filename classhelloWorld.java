ed3ebdb03fb6ce5d26c0ea4817ec84b847a7cb2e
unknown <thorfinnurp@gmail.com>
2017-10-15 17:11:15 +0000

    testa ad setja hello world inn a git

---
+++ classhelloWorld.java
@@ -0,0 +1,9 @@
+class helloWorld
+{
+	 public static void main(String[] args) 
+	 {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
